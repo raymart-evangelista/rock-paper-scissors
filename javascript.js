12dc37db70ae2e4e998805981de39f0f7125148e
raymart-evangelista <raymart.s.evangelista@gmail.com>
2022-03-15 17:34:47 -0700

    add computerPlay function

--- javascript.js
+++ javascript.js
@@ -1,15 +1,28 @@
 // computerPlay function returns Rock Paper or Scissors at random
 
 function computerPlay() {
-    // choose Rock Paper or Scissors at random
-    
+    // choose a random integer
+    let randInt = getRandInt(0,3);
+
+    // match randInt with corresponding rock, paper, or scissors value
+    if (randInt == 0) {
+        let choice = "rock";
+    } else if (randInt == 1) {
+        let choice = "paper";
+    } else {
+        let choice = "scissors";
+    }
+
+    // return the choice
+    return choice;
+
 }
 
 // getRandInt function is a helper for the computerPlay function--used to get a random number between a range of values
 // source function from: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 function getRandInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
+    let min = Math.ceil(min);
+    let max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min);
 }
 
